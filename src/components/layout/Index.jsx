1016af769f8a20ad53dfea71e0656af6625daf6f
Karan11000 <76610086+Karan11000@users.noreply.github.com>
2022-01-08 22:12:48 +0530

    Create Index.jsx

---
+++ src/components/layout/Index.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Tracks from "../tracks/Tracks"
+import Search from "../tracks/Search"
+const Index = ()=>{
+    return(
+       <>
+        <Search></Search>
+        <Tracks></Tracks>
+       </>
+    );
+}
+export default Index;
