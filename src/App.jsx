5abe872c97a0afa58d4df3392ac83d31067739c1
Karan11000 <76610086+Karan11000@users.noreply.github.com>
2022-01-08 18:45:09 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom"
+import Index from "./components/layout/Index"
+import Nav from "./components/layout/Nav"
+import {ContextHandler} from "./Context";
+import Lyrics from './components/tracks/Lyrics'
+
+
+const App = () => {
+  return (
+    <ContextHandler>
+    <BrowserRouter>
+      <>
+      <Nav />
+        <Switch>
+        <Route exact path="/" component={Index}></Route>
+        <Route exact path="/lyrics/track/:id" component={Lyrics} ></Route>
+        </Switch>
+      </>
+    </BrowserRouter>
+    </ContextHandler>
+  );
+}
+export default App
