59172f5b6470d772d1a3b50555030eb91cd53205
Karan11000 <76610086+Karan11000@users.noreply.github.com>
2022-01-08 22:12:54 +0530

    Create Nav.jsx

---
+++ src/components/layout/Nav.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NavBar = ()=>{
+    return(
+       <>
+    <nav className="navbar navbar-dark bg-dark mb-5">
+      <span className="navbar-brand mb-0 h1 mx-auto">Lyric Finder</span>
+    </nav>
+       </>
+    );
+}
+
+export default NavBar;
