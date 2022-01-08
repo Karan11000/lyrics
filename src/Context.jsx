b97b8c4ab3a530a9a91f26a1db8239099794a3ed
Karan11000 <76610086+Karan11000@users.noreply.github.com>
2022-01-08 22:13:14 +0530

    Create Context.jsx

---
+++ src/Context.jsx
@@ -0,0 +1,40 @@
+import React, { createContext, useEffect, useState } from "react"
+import axios from "axios";
+import { getDefaultNormalizer } from "@testing-library/react";
+
+const Context = createContext();
+
+const ContextHandler = ({children}) =>{
+    // console.log(children);
+    const initialData = {
+        tracklist:[],
+        heading:""
+    }
+    const [state, newState] = useState(initialData);
+    useEffect(()=>{
+        // async function getData(){
+        //     const res = await axios.get(`http://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=us&f_has_lyrics=1&apikey=073358cbad4539413d607560a16aa80b`)
+        //     newState({
+        //         tracklist:res.data.message.body.track_list,
+        //         heading:"Top 10 Tracks"
+        //     });
+        // }
+        // getData();
+            axios.get(`https://cors-access-allow.herokuapp.com/http://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=us&f_has_lyrics=1&apikey=073358cbad4539413d607560a16aa80b`)
+            .then((res)=>{
+                console.log("hello", res.data);
+                newState({
+                tracklist: res.data.message.body.track_list,
+                heading: "Top 10 Tracks"
+                });
+            }).catch((err) =>{console.log("hey", err);})
+        }
+    , [])
+    console.log(state);
+    return(
+             <Context.Provider value={[state, newState]}>{children}</Context.Provider> 
+    );
+    
+}
+
+export {Context, ContextHandler};
